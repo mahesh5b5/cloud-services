2a89691b245d3926e7be6343650d008a86cee20f
Mahesh <mk.talanki@gmail.com>
2023-04-13 16:00:16 +0530

    Update subscription.jsx

--- src/pages/subscription.jsx
+++ src/pages/subscription.jsx
@@ -36,12 +36,12 @@ const Subscription = () => {
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
                 <a
-                  href="#"
+                  href="/"
                   className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
                 >
                   Cancel Subscription
                 </a>
-                <a href="#" className="text-sm font-semibold leading-6 text-white">
+                <a href="/" className="text-sm font-semibold leading-6 text-white">
                   Learn more <span aria-hidden="true">→</span>
                 </a>
               </div>
